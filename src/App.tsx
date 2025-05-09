fde7955f02b1625340f446edafbf8a2d15877c16
XiaoChen1027 <93580452+XiaoChen1027@users.noreply.github.com>
2025-05-09 11:27:04 +0800

    Initial commit: Add project files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import DrawingGame from './components/DrawingGame';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <DrawingGame />
+    </div>
+  );
+}
+
+export default App;
